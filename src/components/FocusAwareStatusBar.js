25cd3e393293b962dd882796c219a8755972abbc
Yusuf Ziya YILDIRIM <101952885+yusufziyayildirim@users.noreply.github.com>
2022-10-31 15:50:02 +0300

    Create FocusAwareStatusBar.js

---
+++ src/components/FocusAwareStatusBar.js
@@ -0,0 +1,10 @@
+import { StatusBar } from 'react-native';
+import { useIsFocused } from '@react-navigation/native';
+
+function FocusAwareStatusBar(props) {
+    const isFocused = useIsFocused();
+
+    return isFocused ? <StatusBar {...props} /> : null;
+}
+
+export default FocusAwareStatusBar
