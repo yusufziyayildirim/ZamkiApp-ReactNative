8e962c3cdd81f67e036b954c6d92ad4d59ab975b
Yusuf Ziya YILDIRIM <101952885+yusufziyayildirim@users.noreply.github.com>
2022-10-31 09:31:00 +0300

    Folder Structure

---
+++ src/theme/colors.js
@@ -0,0 +1,13 @@
+const colors = {
+    primary: '',
+    secondary: '',
+    textPrimary: '',
+    textSecondary: '',
+    textGrey: '',
+    backgroundColor: '',
+    white: '',
+    black: '',
+    gray: '',
+}
+
+export default colors;
