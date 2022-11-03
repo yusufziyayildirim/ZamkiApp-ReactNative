2a0151f0a7294bfd492a3c7b84658ebc46e86b97
Yusuf Ziya YILDIRIM <101952885+yusufziyayildirim@users.noreply.github.com>
2022-11-03 15:49:03 +0300

    Update BottomLink.js

--- src/components/Auth/BottomLink.js
+++ src/components/Auth/BottomLink.js
@@ -1,13 +1,27 @@
 import { Text, View, TouchableOpacity } from "react-native";
+import { useDispatch } from "react-redux";
+import { reset } from "../../store/auth/authSlice";
 
 const BottomLink = ({ navigation, message, linkMessage, link }) => {
+
+    const dispatch = useDispatch()
+
+    const handlePress = () => {
+        dispatch(reset())
+        navigation.reset({
+            index: 0,
+            routes: [{ name: link }],
+        });
+        // navigation.navigate()
+    }
+
     return (
 
         <View style={{ flexDirection: "row", justifyContent: "center", marginTop: 45 }}>
             <Text style={{ fontWeight: "600", color: "#5f5f6c", marginRight: 5 }}>
                 {message}
             </Text>
-            <TouchableOpacity onPress={() => navigation.navigate(link)}>
+            <TouchableOpacity onPress={handlePress}>
                 <Text style={{ fontWeight: "600", color: "#8bb2e0" }}>
                     {linkMessage}
                 </Text>
