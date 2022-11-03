f04d6eeb712dda24a0f73e298f4c0cc678dd3c9a
Yusuf Ziya YILDIRIM <101952885+yusufziyayildirim@users.noreply.github.com>
2022-11-03 15:48:21 +0300

    User verify email

---
+++ src/screens/auth/verifyEmail.js
@@ -0,0 +1,73 @@
+import { useState } from "react";
+import { View } from "react-native";
+import routes from "../../constants/routes";
+import axios from "axios";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import ActionButton from "../../components/Auth/ActionButton";
+import HeaderTitle from "../../components/Auth/HeaderTitle";
+import InputBox from "../../components/Auth/InputBox";
+import DismissKeyboard from "../../components/DismissKeyboard";
+import FocusAwareStatusBar from "../../components/FocusAwareStatusBar";
+
+const VerifyEmailScreen = ({ navigation }) => {
+    const [email, setEmail] = useState("")
+    const [loading, setLoading] = useState("")
+    const [message, setMessage] = useState("")
+
+    const resendVerifyEmail = async () => {
+        try {
+            setLoading(true)
+            setMessage("")
+            // configure header's Content-Type as JSON
+            const config = {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            }
+            const response = await axios.post(
+                routes.RESEND_NOTIFICATION,
+                { email },
+                config
+            )
+            if (response.data.status) {
+                setEmail("")
+                setLoading(false)
+                setMessage(response.data.message)
+            }
+
+        } catch (error) {
+            setLoading(false)
+            setMessage(error.response.data.message)
+        }
+    }
+
+    return (
+        <>
+            <FocusAwareStatusBar barStyle="dark-content" />
+            <DismissKeyboard>
+                <View style={{ paddingTop: 50, height: "100%", paddingHorizontal: 7, backgroundColor: "#f2f0fc" }}>
+                    <SafeAreaView>
+                        <HeaderTitle title="Verify Email" subTitle={message} />
+                        <View style={{ marginTop: 15 }}>
+                            <InputBox placeholder="Email" value={email} onChange={setEmail} />
+                        </View>
+                        <ActionButton disabled={loading} value="Send verify email" onPress={resendVerifyEmail} />
+                        <ActionButton
+                            value="Go Back"
+                            onPress={
+                                () => navigation.reset({
+                                    index: 0,
+                                    routes: [{ name: 'Login' }],
+                                })
+                            }
+                        />
+
+                    </SafeAreaView>
+                </View>
+            </DismissKeyboard>
+        </>
+    )
+}
+
+export default VerifyEmailScreen
