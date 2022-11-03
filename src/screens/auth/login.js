6508ee5db352e13572b8dd98d5b30789aedddb1f
Yusuf Ziya YILDIRIM <101952885+yusufziyayildirim@users.noreply.github.com>
2022-11-03 15:48:13 +0300

    User login

--- src/screens/auth/login.js
+++ src/screens/auth/login.js
@@ -1,5 +1,9 @@
+import { useState } from "react";
 import { Text, View, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { useDispatch, useSelector } from "react-redux";
+import { userLogin } from "../../store/auth/authActions";
+import { reset } from "../../store/auth/authSlice";
 
 import FocusAwareStatusBar from "../../components/FocusAwareStatusBar";
 import DismissKeyboard from "../../components/DismissKeyboard";
@@ -8,8 +12,25 @@ import ActionButton from "../../components/Auth/ActionButton";
 import BottomLink from "../../components/Auth/BottomLink";
 import HeaderTitle from "../../components/Auth/HeaderTitle";
 
-
 const LoginScreen = ({ navigation }) => {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const { loading, error } = useSelector(
+        (state) => state.auth
+    )
+
+    const dispatch = useDispatch()
+
+
+    const submitForm = () => {
+        dispatch(userLogin({ email, password }))
+        if (error) {
+            setPassword("")
+        }
+    }
+
     return (
         <>
             <FocusAwareStatusBar barStyle="dark-content" />
@@ -17,19 +38,32 @@ const LoginScreen = ({ navigation }) => {
                 <View style={{ paddingTop: 50, height: "100%", paddingHorizontal: 7, backgroundColor: "#f2f0fc" }}>
                     <SafeAreaView>
                         <HeaderTitle title="Hello Again!" subTitle={`Wellcome back you've \n been missed!`} />
+                        {error && (
+                            <Text style={{ color: "red", textAlign: "center", marginTop: 15 }} >{error}</Text>
+                        )}
                         <View style={{ paddingHorizontal: 25, marginTop: 25 }}>
-                            <InputBox placeholder="Email" />
-                            <InputBox placeholder="Password" />
+                            <InputBox placeholder="Email" value={email} onChange={setEmail} />
+                            <InputBox placeholder="Password" value={password} onChange={setPassword} />
 
-                            <View style={{ alignItems: "flex-end", marginBottom:10 }}>
-                                <TouchableOpacity onPress={() => navigation.navigate('ForgotPassword')}>
+                            <View style={{ alignItems: "flex-end", marginBottom: 10 }}>
+                                <TouchableOpacity onPress={() => { navigation.navigate('ForgotPassword'); dispatch(reset()) }}>
                                     <Text style={{ fontWeight: "500", color: "#5f5f6c", marginRight: 5, textAlign: "right", paddingRight: 5 }}>
                                         Forgot Password
                                     </Text>
                                 </TouchableOpacity>
                             </View>
 
-                            <ActionButton value="Sıgn In" />
+                            <ActionButton disabled={loading} value="Sıgn In" onPress={submitForm} />
+                            {error == "Verify e-mail address" && (
+                                <ActionButton disabled={loading} value="Verify Email"
+                                    onPress={
+                                        () => navigation.reset({
+                                            index: 0,
+                                            routes: [{ name: 'VerifyEmail' }],
+                                        })
+                                    }
+                                />
+                            )}
                         </View>
 
                         <BottomLink navigation={navigation} message="Not a member?" linkMessage="Register now" link="Register" />
