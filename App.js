91a376b893fbc70e7280ddc58a02718280557fee
Yusuf Ziya YILDIRIM <101952885+yusufziyayildirim@users.noreply.github.com>
2022-11-03 15:56:22 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
-import Auth from "./src/navigation/auth";
+import { Provider } from "react-redux";
+import AppRoute from "./src/navigation/AppRoute";
+import { store } from "./src/store";
 
 export default function App() {
   return (
-    <Auth />
+    <Provider store={store}>
+      <AppRoute />
+    </Provider>
   );
 }
 
