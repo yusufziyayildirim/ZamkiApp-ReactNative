196936fde17375cf46df47de356b45520270620a
Yusuf Ziya YILDIRIM <101952885+yusufziyayildirim@users.noreply.github.com>
2022-11-07 15:25:40 +0300

    Update AppNavigator.js

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,14 +1,21 @@
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import TabBar from '../containers/TabBar';
 import HomeScreen from '../screens/app/home';
+import ChatsScreen from '../screens/app/chats';
+import RoomsScreen from '../screens/app/rooms';
+import ProfileScreen from '../screens/app/profile';
 
-const AppStack = createNativeStackNavigator();
+
+const TabNavigator = createBottomTabNavigator();
 
 const AppNavigator = () => {
     return (
-        <AppStack.Navigator initialRouteName="Home">
-            <AppStack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
-        </AppStack.Navigator>
+        <TabNavigator.Navigator initialRouteName="Home" tabBar={props => <TabBar {...props} />}>
+            <TabNavigator.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} /> 
+            <TabNavigator.Screen name="Chats" component={ChatsScreen} options={{ headerShown: false }} /> 
+            <TabNavigator.Screen name="Rooms" component={RoomsScreen} options={{ headerShown: false }} /> 
+            <TabNavigator.Screen name="Profile" component={ProfileScreen} options={{ headerShown: false }} /> 
+        </TabNavigator.Navigator>
     )
 }
 
