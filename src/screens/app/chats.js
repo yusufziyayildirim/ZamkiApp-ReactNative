d2bb5e214f9db3f5db6b53258029b2599a5f1f95
Yusuf Ziya YILDIRIM <101952885+yusufziyayildirim@users.noreply.github.com>
2022-11-14 16:02:18 +0300

    Update chats.js

--- src/screens/app/chats.js
+++ src/screens/app/chats.js
@@ -1,12 +1,28 @@
-import { View, ScrollView, Text } from 'react-native'
+import { View, ScrollView, Text, TouchableOpacity, Button } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import DismissKeyboard from '../../components/DismissKeyboard'
 import FocusAwareStatusBar from '../../components/FocusAwareStatusBar'
 import ChatListItem from '../../components/ChatListItem';
 import SearchBox from '../../components/SearchBox';
 import colors from '../../theme/colors';
+import { collection, onSnapshot, where, query } from "firebase/firestore";
+import { useEffect, useState } from 'react';
+import { db } from '../../constants/firebase';
+import { useSelector } from 'react-redux';
 
-const ChatsScreen = () => {
+const ChatsScreen = ({ navigation }) => {
+
+    const [chats, setChats] = useState([]);
+    const { userInfo } = useSelector(state => state.auth);
+    // console.log(userInfo[0])
+    const colRef = query(collection(db, "chats"), where('users', 'array-contains', userInfo.email))
+    useEffect(() => {
+        return onSnapshot(colRef, (snapshot) => {
+            setChats(snapshot.docs)
+        })
+    }, [])
+
+    console.log(chats);
 
     return (
         <View style={{ backgroundColor: colors.backgroundColor, paddingTop: 10 }}>
@@ -18,30 +34,29 @@ const ChatsScreen = () => {
                     <ScrollView style={{ height: "102.5%" }}>
                         <SearchBox />
                         <View>
-                            <ChatListItem
+                            {
+                                chats.map((item, i) => {
+                                    console.log(item.data())
+                                    if (item.data().messages[0]) {
+                                        return (
+                                            <TouchableOpacity key={i} onPress={() => navigation.navigate('Chat', { chatId: item.id })}>
+                                                <ChatListItem
+                                                    userImg="https://seamcline.com.ng/students/assets/images/users/1.jpg"
+                                                    userName={item.data().usersName[item.data().users.findIndex(x => x != userInfo.email)]}
+                                                    userMessage={item.data().messages[0].text}
+                                                    />
+                                            </TouchableOpacity>
+                                        )
+                                    }
+                                })
+                            }
+                            {/* <ChatListItem
                                 userImg="https://seamcline.com.ng/students/assets/images/users/1.jpg"
+                                userName="Abdurrahman Cüce"
+                                userMessage="Test mesajı"
                                 newMessage={true}
                                 totalMessage={4}
-                                userName="Abdurrahman Cüce"
-                                userMessage="Test mesajı"
-                            />
-                            <ChatListItem
-                                userImg="https://seamcline.com.ng/students/assets/images/users/1.jpg"
-                                userName="Yusuf Yıldırım"
-                                userMessage="Test mesajı"
-                            />
-                            <ChatListItem
-                                userImg="https://seamcline.com.ng/students/assets/images/users/1.jpg"
-                                newMessage={true}
-                                totalMessage={1}
-                                userName="Yusuf Ziya Yıldırım"
-                                userMessage="Test mesajı"
-                            />
-                            <ChatListItem
-                                userImg="https://seamcline.com.ng/students/assets/images/users/1.jpg"
-                                userName="Abdurrahman Cüce"
-                                userMessage="Test mesajı"
-                            />
+                            /> */}
                         </View>
                     </ScrollView>
                 </SafeAreaView>
