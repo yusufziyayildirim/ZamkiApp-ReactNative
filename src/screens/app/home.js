3134ec3e65003fda1ece363b9c56c42041e945b9
Yusuf Ziya YILDIRIM <101952885+yusufziyayildirim@users.noreply.github.com>
2022-11-14 16:01:38 +0300

    Get all user data

--- src/screens/app/home.js
+++ src/screens/app/home.js
@@ -6,8 +6,44 @@ import FocusAwareStatusBar from '../../components/FocusAwareStatusBar'
 import UserListItem from '../../components/UserListItem';
 import SearchBox from '../../components/SearchBox';
 import colors from '../../theme/colors';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import routes from '../../constants/routes';
+import { useSelector } from 'react-redux';
+import axios from 'axios';
 
-const HomeScreen = ({navigation}) => {
+const HomeScreen = ({ navigation }) => {
+  const { userToken } = useSelector(state => state.auth);
+
+  const [loading, setLoading] = useState([])
+  const [users, setUsers] = useState([])
+  const getAllUser = async () => {
+    try {
+      setLoading(true)
+      // configure header's Content-Type as JSON
+      const config = {
+        headers: {
+          Authorization: `Bearer ${userToken}`,
+        },
+      }
+      const response = await axios.get(
+        routes.GET_ALL_USER,
+        config
+      )
+      setLoading(false)
+      setUsers(response.data.data)
+
+    } catch (error) {
+      console.log(error)
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    getAllUser()
+  }, [])
+
+
 
   return (
     <View style={{ backgroundColor: colors.backgroundColor, paddingTop: 10 }}>
@@ -16,26 +52,24 @@ const HomeScreen = ({navigation}) => {
         <SafeAreaView>
           <Text style={{ fontSize: 25, fontWeight: "800", textAlign: "center" }}>Community</Text>
           <View style={{ width: "100%", height: .5, backgroundColor: colors.borderColor, marginTop: 5 }} />
-          <ScrollView s>
+          <ScrollView>
             <SearchBox />
             <View>
-              <UserListItem
-                userImg="https://seamcline.com.ng/students/assets/images/users/1.jpg"
-                userName="Yusuf Yıldırım"
-                userDetail="I want to practice basic spanis and make some friends"
-                speaks=""
-                learns=""
-                navigation={navigation}
-              />
-              <UserListItem
-                userImg="https://seamcline.com.ng/students/assets/images/users/1.jpg"
-                userName="Yusuf Yıldırım"
-                userDetail="I want to practice basic spanis and make some friends"
-                speaks=""
-                learns=""
-                navigation={navigation}
-              />
-              
+              {users &&
+                users.map((user, i) => {
+                  return (
+                    <TouchableOpacity key={i} onPress={() => navigation.navigate('UserDetail', { user: user })}>
+                      <UserListItem
+                        userImg="https://seamcline.com.ng/students/assets/images/users/1.jpg"
+                        userName={user.name}
+                        userDetail={user.desc}
+                        speaks=""
+                        learns=""
+                      />
+                    </TouchableOpacity>
+                  )
+                })
+              }
             </View>
           </ScrollView>
         </SafeAreaView>
