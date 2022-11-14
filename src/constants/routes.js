d748d69b7216e0f5246ea42c86ce41c0b6adcf5c
Yusuf Ziya YILDIRIM <101952885+yusufziyayildirim@users.noreply.github.com>
2022-11-14 16:00:47 +0300

    Update routes.js

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,5 +1,5 @@
 
-const BASE_URL = "http://192.168.1.41:8000/api";
+const BASE_URL = "http://10.254.112.61:8000/api";
 const routes = {
     LOGIN: BASE_URL + "/login",
     LOGOUT: BASE_URL + "/logout",
@@ -8,6 +8,7 @@ const routes = {
     RESEND_NOTIFICATION: BASE_URL + "/email/verification-notification",
     RESET_PASSWORD_MAIL: BASE_URL + "/send-reset-password-email",
     CHANGE_PASSWORD: BASE_URL + "/changepassword",
+    GET_ALL_USER: BASE_URL + "/getalluser",
 }
 
 export default routes;
